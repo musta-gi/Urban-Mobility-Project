bd2bee44b20d5b6864cc48e276572d645c87bf0b
Mustapha RACHDI <mustapha.rachdi1@grenoble-inp.org>
2024-01-11 09:45:00 +0100

    loading database in python using rpy2 - failed

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import numpy as np
+import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri
 
-def array_to_csv(array, filename):
-    df = pd.DataFrame(array)
-    df.to_csv(filename, index=False, header=False)
+pandas2ri.activate()
+ro.r['load']('allgre.PB_V2.RData')
+r_dataframe = ro.r['allgre.PB_V2']
+# Convert the R DataFrame into a Pandas DataFrame
+df = pandas2ri.ri2py(r_dataframe)
+
+# Now 'df' is a Pandas DataFrame
+print(df.head())
